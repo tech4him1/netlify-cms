4f86fd873bd4239bb0bfa26f623a5c287c546d9c
Caleb <inchristalway-tech4him@yahoo.com>
2018-10-10 21:12:09 -0600

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -71,4 +71,11 @@ const plugins = () => {
 module.exports = {
   presets: presets(),
   plugins: plugins(),
+  babelrcRoots: [
+    // Keep the root as a root
+    ".",
+
+    // Also consider monorepo packages "root" and load their .babelrc files.
+    "./packages/*"
+  ],
 };
