d608dc4eece18a9948a1a4e7b3fa52c926a2f290
Caleb <inchristalway-tech4him@yahoo.com>
2018-10-10 21:14:19 -0600

    Update and rename babel.config.js to .babelrc.js

--- packages/netlify-cms-core/.babelrc.js
+++ packages/netlify-cms-core/.babelrc.js
@@ -1,10 +1,7 @@
 const path = require('path');
-const babelConfig = require('../../babel.config.js');
 
 module.exports = {
-  ...babelConfig,
   plugins: [
-    ...babelConfig.plugins,
     'react-hot-loader/babel',
     [
       'module-resolver',
